86bd3ed50391c725ad6ee9bd39aefc10e4dec318
Zak Ward <zakward85@gmail.com>
2022-03-31 17:02:39 -0500

    about me img fix

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <>
             <div className = "about-header-container">
-                <img className = "about-img" alt = "new drink" src = "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/71578934_10104375353323867_2926410438537117696_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=vtZpxiMNAvIAX_CeaXa&_nc_ht=scontent-dfw5-1.xx&oh=00_AT-SKLQIWIXCOU60MoL6IsOKwfvKbk2F973tNuenLnMhZQ&oe=6227E594"/>
+                {/* <img className = "about-img" alt = "Zak" src = "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/71578934_10104375353323867_2926410438537117696_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=vtZpxiMNAvIAX_CeaXa&_nc_ht=scontent-dfw5-1.xx&oh=00_AT-SKLQIWIXCOU60MoL6IsOKwfvKbk2F973tNuenLnMhZQ&oe=6227E594"/> */}
                 <h1 className = "about-title">The Cocktail Connoisseur</h1>
                 <p className = "about-description">The Cocktail Connoisseur App was created by Zak Ward as part of the VSchool Level 4 Captstone Project. The project focus is on the planning, organizing, and creation of a responsive React App that allows the User to interact with an online API to get specific data requests from the CocktailDB.</p>
                 <hr className = "line-break"/>
